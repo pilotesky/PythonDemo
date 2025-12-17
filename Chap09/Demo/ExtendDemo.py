92640170a95dfe1152a6e36cb240051a4973bf2d
pilotesky <pilotechen@gmail.com>
2025-12-17 15:10:58 +0800

    update

--- Chap09/Demo/ExtendDemo.py
+++ Chap09/Demo/ExtendDemo.py
@@ -1,60 +1,63 @@
 # 组合
-class Ranking():
-    def __init__(self,world_rank=0,national_rank=0):
+class Ranking:
+
+    def __init__(self,world_rank,national_rank):
         self.world_rank = world_rank
         self.national_rank = national_rank
 
-    def show_ranking(self):
-        print(f'这所学校在国际上排名{self.world_rank}名,在国内排名{self.national_rank}')
+    def describe_rank(self):
+        print(f'这所学校在全球的排名是第{self.world_rank},在国内的全球排名是{self.national_rank}')
 
 # 父类
-class University():
+class University:
 
-    # 初始化属性
-    def __init__(self,title,location,world_rank=0,national_rank=0):
+    def __init__(self,title,location,ranking):
         self.title = title
         self.location = location
-        self.ranking = Ranking(world_rank,national_rank)
+        self.ranking = ranking
     
-    # 介绍
     def describe_university(self):
-        print(f'这所学校的名称是{self.title},是在{self.location},这所学校在国际上排名是{self.ranking.world_rank},在国内的排名是{self.ranking.national_rank}')
+        print(f'这所学校的全称是{self.title},位于{self.location},其全球排名是第{self.ranking.world_rank}名,其国内排名是第{self.ranking.national_rank}名')
 
-
-# 子类 私立大学
-class Private_University(University):
-
-    # 继承父类初始化
-    def __init__(self, title, location,world_rank=0,national_rank=0):
-        super().__init__(title,location,world_rank,national_rank)
-
-    # 自己的方法来介绍
-    def describe_private_univeristy(self):
-        print(f'这是一所私立大学,学校名称是{self.title},是在{self.location},这所学校在国际上排名是{self.ranking.world_rank},在国内的排名是{self.ranking.national_rank}')
-    # 重写父类的方法
-    def describe_university(self):
-        print(f'{self.title}是一所顶级学校')
-
-# 子类 公立大学
+# 子类
 class Public_University(University):
 
-    # 继承父类初始化
-    def __init__(self,title,location,world_rank=0,national_rank=0):
-        super().__init__(title,location,world_rank,national_rank)
+    def __init__(self, title, location,ranking):
+        super().__init__(title, location,ranking)
 
-    # 自己的方法来介绍
     def describe_public_university(self):
-        print(f'这是一所公立大学,学校名称是{self.title},是在{self.location},这所学校在国际上排名是{self.ranking.world_rank},在国内的排名是{self.ranking.national_rank}')
-    # 重写父类的方法
+        print(f'这所公立大学名称是:{self.title},位于{self.location},其全球排名是第{self.ranking.world_rank}名,其国内排名是第{self.ranking.national_rank}名')
+    # 重写父类方法
     def describe_university(self):
-        print(f'{self.title}是一所顶级学校')
+        print('这是一所顶级公立大学')
 
-Caltech = Private_University('加州理工学院','美国',2,3)
-Caltech.describe_private_univeristy()
-# 重写父类的方法
+class Private_University(University):
+
+    def __init__(self, title, location,ranking):
+        super().__init__(title, location,ranking)
+
+    def describe_private_university(self):
+        print(f'这所私立大学名称是:{self.title},位于{self.location},其全球排名是第{self.ranking.world_rank}名,其国内排名是第{self.ranking.national_rank}名')
+
+    # 重写父类方法
+    def describe_university(self):
+        print('这是一所顶级私立大学')
+
+# 调用父类
+fudan_ranking = Ranking(43,3)
+FDU = University('复旦大学','中国上海',fudan_ranking)
+FDU.describe_university()
+
+# 调用子类 公立大学
+ucla_ranking = Ranking(27,7)
+UCLA = Public_University('加州大学洛杉矶分校','美国加州',ucla_ranking)
+UCLA.describe_public_university()
+# 重写父类方法
+UCLA.describe_university()
+
+# 调用子类 私立大学
+caltech_ranking = Ranking(6,6)
+Caltech = Private_University('加州理工学院','美国加州',caltech_ranking)
+Caltech.describe_private_university()
+# 重写父类方法
 Caltech.describe_university()
-
-UCL = Public_University('伦敦大学国王学院','英国',5,2)
-UCL.describe_public_university()
-# 重写父类的方法
-UCL.describe_university()
